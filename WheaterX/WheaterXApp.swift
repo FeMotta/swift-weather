0b83bef88b5e3454277da6c2466ae289c6749512
Fernando Motta <fernandosantosmotta@gmail.com>
2022-08-07 21:59:31 -0300

    Initial Commit

---
+++ WheaterX/WheaterXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheaterXApp.swift
+//  WheaterX
+//
+//  Created by Fernando dos Santos Motta on 07/08/22.
+//
+
+import SwiftUI
+
+@main
+struct WheaterXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
